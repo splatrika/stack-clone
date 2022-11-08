79b965f505ec2d0e5f6a23bb61ada019a44b29e5
daniilbelov <tseflutty@yandex.ru>
2022-11-08 18:33:33 +0300

    Add start screen ui

---
+++ Assets/Game/Source/UI/StartScreenUI.cs
@@ -0,0 +1,41 @@
+using Splatrika.StackClone.Model;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Splatrika.StackClone.UI
+{
+    public class StartScreenUI : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _run;
+
+        private IGameLifeService _game;
+
+
+        [Inject]
+        public void Construct(IGameLifeService game)
+        {
+            _game = game;
+        }
+
+
+        private void Start()
+        {
+            _run.onClick.AddListener(OnRunClicked);
+        }
+
+
+        private void OnRunClicked()
+        {
+            _game.Run();
+            gameObject.SetActive(false);
+        }
+
+
+        private void OnGameReseted()
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
