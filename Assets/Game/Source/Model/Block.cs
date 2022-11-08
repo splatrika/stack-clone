11982574047fa4b78a0240bd352d637c380ff8ac
daniilbelov <tseflutty@yandex.ru>
2022-11-08 16:56:15 +0300

    Update block: add inverse cut

--- Assets/Game/Source/Model/Block.cs
+++ Assets/Game/Source/Model/Block.cs
@@ -25,5 +25,28 @@ namespace Splatrika.StackClone.Model
             cuttedRect.yMin = Mathf.Max(a.yMin, b.yMin);
             return new Block(cuttedRect, Color);
         }
+
+
+        public Block InverseCut(Rect cutted)
+        {
+            var result = new Rect();
+            result.xMin = (Rect.xMax == cutted.xMax)
+                ? Rect.xMin
+                : cutted.xMax;
+            result.xMax = (Rect.xMin == cutted.xMin)
+                ? Rect.xMax
+                : cutted.xMin;
+            result.yMin = (Rect.yMax == cutted.yMax)
+                ? Rect.yMin
+                : cutted.yMax;
+            result.yMax = (Rect.yMin == cutted.yMin)
+                ? Rect.yMax
+                : cutted.yMin;
+            if (Rect == cutted)
+            {
+                result = new Rect(0, 0, 0, 0);
+            }
+            return new Block(result, Color);
+        }
     }
 }
